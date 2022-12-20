895f15f4a1b6d7cef5088513f57b3cac458c5fe9
Liza <el.nik2017@ya.ru>
2022-12-20 11:34:45 +0300

    Звдача 2 решена

--- Homework1/task2/Program.cs
+++ Homework1/task2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+WriteLine ("Введите число a:");
+int a = int.Parse(ReadLine());
+WriteLine ("Введите число b:");
+int b = int.Parse(ReadLine());
+int max = a;
+int min = b;
+
+if (a>max) max=a;
+if (b>max) max=b;
+
+Write ("max=");
+WriteLine(max);
+
+if (a<min) min=a;
+if (b<min) min=b;
+
+Write ("min=");
+WriteLine(min);
+

b28049c54050b24f42b6222a8f1b431cde35f53c
Liza <el.nik2017@ya.ru>
2022-12-24 18:11:51 +0300

    Задача 13  решена

--- Homework2/task13/Program.cs
+++ Homework2/task13/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+Write ("Введите число a:");
+int a = int.Parse(ReadLine());
+int max =999;
+while (a> max)  a=a/10;
+int b=a%10;
+
+WriteLine (b>0 && a>99? b:"третьей цифры нет");
+
+
+
+
+

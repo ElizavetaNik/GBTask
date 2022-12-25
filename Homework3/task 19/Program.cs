7727306f620220490f8333d5eaf26ff8cf822212
Liza <el.nik2017@ya.ru>
2022-12-25 12:33:49 +0300

    Задача 19-2

--- Homework3/task 19/Program.cs
+++ Homework3/task 19/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+Write ("Введите число n:");
+int n = Convert.ToInt32 (ReadLine());
+
+int a = n/10000;
+int b = n/1000%10;
+int c = n%100/10;
+int d = n%10;
+WriteLine (a==d && b==c ? "ДА":"НЕТ");
+

7fdbb39ea2a2a783e402152d2e26028fe9b417b9
Liza <el.nik2017@ya.ru>
2023-01-18 21:56:09 +0300

    Added task 29

--- Homework4/task27/Program.cs
+++ Homework4/task27/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+Write ("Введите число a:");
+int numberA = int.Parse(ReadLine());
+int b = Numberlen(numberA);
+SUM (numberA,b);
+int Numberlen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+void SUM(int n, int b)
+{
+    int result = 0;
+    for (int i=1; i <= b; i++)
+    {
+        result = result+n%10;
+        n/=10;
+        
+    }
+    WriteLine(result);
+
+}

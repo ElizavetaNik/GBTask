c5593c7a99c77e810ee4d08d2efe76f57e4cf0ee
Liza <el.nik2017@ya.ru>
2023-01-18 22:23:50 +0300

    Решена task 29

--- Homework4/task29/Program.cs
+++ Homework4/task29/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+int [] rendomArray = new int[8];
+for (int i = 0; i < rendomArray.Length;i++)
+{
+    rendomArray[i] = new
+    Random().Next(1,9);
+     Write(rendomArray[i]+" ");
+
+}
+
+    
+

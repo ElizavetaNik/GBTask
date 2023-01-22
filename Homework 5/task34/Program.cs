7080051291198102d47a3216d1a64ba717d10640
Liza <el.nik2017@ya.ru>
2023-01-22 20:26:44 +0300

    Решено task 34

--- Homework 5/task34/Program.cs
+++ Homework 5/task34/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+Write ("Введите размер массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] numbers = new int [size];
+int count = 0;
+ArrayRandom (numbers);
+PrintArray(numbers);
+
+for (int i=0; i < numbers.Length; i++)
+{
+    if (numbers[i] % 2 == 0)
+    count++;
+}
+WriteLine($"четных счисел -> {count}");
+
+
+void ArrayRandom (int [] array)
+{
+    
+    for (int i=0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next (100,1000);      
+        
+    }
+   
+}
+void PrintArray(int [] array)
+{
+    for (int i=0; i < array.Length; i++)
+    {
+        Write(array[i] + " ");
+    }
+    WriteLine(); 
+}

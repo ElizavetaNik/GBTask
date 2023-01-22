cf8f6cb974889f4477f6fd18be8084bff80a6544
Liza <el.nik2017@ya.ru>
2023-01-22 20:46:35 +0300

    Решeна task 36

--- Homework 5/task36/Program.cs
+++ Homework 5/task36/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+Write ("Введите размер массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] numbers = new int [size];
+
+ArrayRandom (numbers);
+PrintArray(numbers);
+
+int Sum = 0;
+for (int i = 1; i < numbers.Length; i+= 2)
+{
+   Sum  += numbers[i]; 
+}
+Write(Sum);
+
+
+void ArrayRandom (int [] array)
+{
+    
+    for (int i=0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next (-100,101);      
+        
+    }
+   
+}
+void PrintArray(int [] array)
+{
+    for (int i=0; i < array.Length; i++)
+    {
+        Write(array[i] + " ");
+    }
+    WriteLine(); 
+}

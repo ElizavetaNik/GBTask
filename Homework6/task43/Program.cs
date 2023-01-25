c0e2e5963bf5e357b409736fd691210608cc91fe
Liza <el.nik2017@ya.ru>
2023-01-25 20:52:58 +0300

    Решено task 43

--- Homework6/task43/Program.cs
+++ Homework6/task43/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+WriteLine ("Введите b1:");
+double b1 = Convert.ToInt32(Console.ReadLine());
+WriteLine ("Введите k1:");
+double k1 = Convert.ToInt32(Console.ReadLine());
+WriteLine ("Введите b2:");
+double b2 = Convert.ToInt32(Console.ReadLine());
+WriteLine ("Введите k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2+b1)/(-k1+k2);
+double y = k2*x+b2;
+
+WriteLine ($"{x},{y}");
+
+
+
+
+

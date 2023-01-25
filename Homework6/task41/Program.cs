71a44fd45991bf5ea19c876eea0a74b8354c5b56
Liza <el.nik2017@ya.ru>
2023-01-25 20:02:31 +0300

    Решена  task 41

--- Homework6/task41/Program.cs
+++ Homework6/task41/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+Write ("Введите числа:");
+int[]numbers = StringToNum(Console.ReadLine());
+PrintArray(numbers);
+int Sum = 0;
+for (int i = 1; i < numbers.Length; i++)
+{  if (numbers[i]>0)
+
+{
+     Sum++;
+}
+   
+}
+ WriteLine();
+ WriteLine($"-> = {Sum}");
+
+int[] StringToNum(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length;i++)
+    {
+        if (input[i] == ',')
+        {
+            count++;
+        }
+    }
+
+int[]numbers = new int [count];
+int index = 0;
+
+for (int i = 0; i < input.Length;i++)
+{
+    string temp = "";
+
+    while (input[i] !=',' )
+    {
+        if (i != input.Length-1)
+        {
+            temp +=input[i].ToString();
+            i++;
+        }
+        else
+        {
+           temp +=input[i].ToString();
+            break; 
+        }
+
+    }
+    numbers[index] = Convert.ToInt32(temp);
+    index++;
+}
+ return numbers;
+}
+
+void PrintArray(int [] array)
+{
+    Write("["); 
+    for (int i=0; i < array.Length-1; i++)
+    {
+        Write($"{array[i]},");
+    }
+    Write($"{array[array.Length-1]}] "); 
+}

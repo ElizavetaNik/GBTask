be16af8847abdd410fb94f8ffec5e569785b07a9
Liza <el.nik2017@ya.ru>
2023-01-27 22:54:04 +0300

    Решена task 49

--- Homework 7/task49/Program.cs
+++ Homework 7/task49/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+Write("Введите размер матрицы:");
+string[]parmaters=ReadLine()!.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+
+double[,] array=GetMatrixArray(int.Parse(parmaters[0]),int.Parse(parmaters[1]));
+PrintMatrixArray(array);
+
+double[,] GetMatrixArray(int rows, int columns)
+{
+    double[,] resultArray=new double[rows,columns]; 
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns  ; j++)
+        {
+            resultArray [i,j]=Convert.ToDouble(new Random().Next(-100,100)) / 10; 
+        }
+    }
+     return resultArray;
+}
+
+
+void PrintMatrixArray(double[,] inArray)
+{
+     for(int i = 0; i < inArray.GetLength(0); i++)
+     {
+       for(int j = 0; j < inArray.GetLength(1); j++)
+       {
+        Write ($"{inArray [i,j],5}");
+       }
+       WriteLine();
+     }
+} 

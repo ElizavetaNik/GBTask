1d3f881047f8e84bc1c26786369daa0585ebed86
Liza <el.nik2017@ya.ru>
2023-02-04 21:48:11 +0300

    Р  task 66

--- H9-1/task66/Program.cs
+++ H9-1/task66/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumFromMToN(m, n);
+
+
+void SumFromMToN(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+
+
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}

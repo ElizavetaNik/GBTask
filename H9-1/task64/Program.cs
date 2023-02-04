2a9feccbaff7a7a1027830d9466c0b6dcd0b4a5b
Liza <el.nik2017@ya.ru>
2023-02-04 21:30:14 +0300

    Решена  task 64

--- H9-1/task64/Program.cs
+++ H9-1/task64/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Clear();
+
+Write ("Введите число N:");
+int numberN = Convert.ToInt32 (ReadLine());
+int count = 1;
+PrintNumbers(numberN,count);
+
+
+void PrintNumbers(int num, int count)
+{
+    
+    if ( count>num)
+    {
+        return;
+    }
+    else
+    {
+       
+       PrintNumbers(num,count+1);
+       Write (count + " "); 
+    }
+   
+
+}
